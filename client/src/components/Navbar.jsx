eb4a07becdb7931625462cbac4f629d3d085526d
Muniox <muniox20@gmail.com>
2022-11-21 23:07:14 +0100

    react blog

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import Logo from "../img/logo.png"
+import {Link} from "react-router-dom";
+
+export const Navbar = () => {
+    return (
+        <div className='navbar'>
+            <div className='container'>
+                <div className="logo">
+                    <img src={Logo} alt=""/>
+                </div>
+                <div className="links">
+                    <Link to="/?cat=art" className='link'>
+                        <h6>ART</h6>
+                    </Link>
+                    <Link to="/?cat=science" className='link'>
+                        <h6>SCIENCE</h6>
+                    </Link>
+                    <Link to="/?cat=technology" className='link'>
+                        <h6>TECHNOLOGY</h6>
+                    </Link>
+                    <Link to="/?cat=cinema" className='link'>
+                        <h6>CINEMA</h6>
+                    </Link>
+                    <Link to="/?cat=design" className='link'>
+                        <h6>DESIGN</h6>
+                    </Link>
+                    <Link to="/?cat=food" className='link'>
+                        <h6>FOOD</h6>
+                    </Link>
+                    <span>John</span>
+                    <span>Logout</span>
+                    <span className="write">
+                        <Link to="/write" className="link">Write</Link>
+                    </span>
+                </div>
+            </div>
+        </div>
+    )
+}
