96e66ee024f887199eddacf3e14f2863c534499a
Muniox <muniox20@gmail.com>
2023-01-08 23:27:49 +0100

    fetch mysql data

--- client/src/pages/Single.jsx
+++ client/src/pages/Single.jsx
@@ -1,35 +1,55 @@
-import React from "react";
+import React, {useContext, useEffect, useState} from "react";
 import Edit from "../img/edit.png";
 import Delete from "../img/delete.png";
-import {Link} from "react-router-dom";
+import {Link, useLocation} from "react-router-dom";
 import {Menu} from "../components/Menu"
+import axios from "axios";
+import moment from "moment";
+import {AuthContext} from "../context/authContext"; //better is date-fns
 
 export const Single = () => {
+    const [post, setPost] = useState({});
+
+    const location = useLocation();
+
+    const postId = location.pathname.split("/")[2];
+
+    const {currentUser} = useContext(AuthContext)
+
+    useEffect(() => {
+        (async () => {
+            try{
+                const res = await axios.get(`api/posts/${postId}`);
+                console.log(res.data)
+                setPost(res.data)
+            }catch (error) {
+                console.log(error);
+            }
+        })();
+    },[postId]);
     return (
         <div className="single">
             <div className="content">
-                <img src="" alt=""/>
+                <img
+                    src={post?.img}
+                    alt=""
+                />
                 <div className="user">
                     <img src="" alt=""/>
 
                     <div className="info">
-                        <span>John</span>
-                        <p>posted 2 days ago</p>
+                        <span>{post.username}</span>
+                        <p>Posted {moment(post.date).fromNow()} </p>
                     </div>
-                    <div className="edit">
+                    {currentUser.username === post.username && <div className="edit">
                         <Link to={`/write?edit=2`}>
                             <img src={Edit} alt=""/>
                         </Link>
                         <img src={Delete} alt=""/>
-                    </div>
+                    </div>}
                 </div>
-                <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit</h1>
-                <p>
-                    Where does it come from?
-                    Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.
-
-                    The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.
-                </p>
+                <h1>{post.title}</h1>
+                {post.desc}
             </div>
             <Menu />
         </div>
